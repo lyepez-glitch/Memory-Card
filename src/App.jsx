74d7cf6e2dc63af414938a9c8ccfc3226cde1d60
Lucas Yepez <lucasyepez99@gmail.com>
2024-06-27 23:07:28 -0500

    added score functionality

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ function App() {
 
   const[total,setTotal] = useState(0);
 
+  const[clicked,setClick] = useState([])
+  const [bestScore,setBestScore] = useState(0)
+
   // const firstPokemon = await getPokemon()
 
   // console.log('first pokemon',firstPokemon)
@@ -40,25 +43,28 @@ function App() {
 
      },[])
 
-  //    useEffect(() => {
-  //     const cards = document.querySelectorAll('.card');
-  //     let counter = 0;
-  //     cards.forEach((card)=>{
-  //       counter++;
-  //       if(counter > 12){
-  //         card.style.display = 'none'
-  //       }
-  //     })
-  //  },[pokemons]);
 
 
 
 
-  function handleCardClick(){
-    console.log('clicked')
+  function handleCardClick(e){
+    console.log('clicked',e.target.name)
     const shuffledPokemon = shuffle(pokemons);
     setPokemons(shuffledPokemon.slice(0,12))
-    setTotal(total + 1);
+    if(clicked.includes(e.target.name)){
+      if(total === 4){
+        setTotal(0)
+      }else{
+        setTotal(total + 1);
+      }
+      if(total > bestScore){
+        setBestScore(total)
+      }
+
+    }else{
+      setClick(...clicked,e.target.name)
+    }
+
   }
 
 
@@ -66,10 +72,9 @@ function App() {
     <>
     <h1 id="title">Pokemon Memory Game</h1>
     <p>Get points by clicking on an image but don't click on any more than once!</p>
-    <div id="score">Score:</div>
-    <div>Best score:</div>
+    <div id="score">Score:{total}</div>
+    <div>Best score:{total > 0? bestScore + 1:bestScore}</div>
     <div className="cardContainer">
-    <div>Number of changes: {total}</div>
      {console.log('count',total,pokemons)}
       { pokemons.map((pokemon,index) =>
       {
